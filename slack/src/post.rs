1a33544a97a3e361faaecfd1f91b313516966db0
Yourein <rein.you.3.1@gmail.com>
2024-09-16 17:55:24 +0900

    Implement to_json partially

--- slack/src/post.rs
+++ slack/src/post.rs
@@ -14,7 +14,21 @@ impl<'a> Post<'_> {
     }
 
     pub fn to_json(&mut self) -> String {
-        todo!()
+        let blocks_json: String = self.blocks
+            .iter()
+            .filter_map(|x| {
+                match x {
+                    Blocks::TextBlock(textblock) => {
+                        serde_json::to_string(textblock).ok()
+                    },
+                }
+            })
+            .collect::<Vec<String>>()
+            .join(",");
+
+        //TODO: Section
+
+        format!{"{{blocks: [{}]}}", blocks_json}
     }
 
     pub fn add_text_block(&mut self, text: &str) {
@@ -24,5 +38,20 @@ impl<'a> Post<'_> {
                 TextBlock::new(str)
             )
         );
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Post;
+
+    #[test]
+    fn json() {
+        let mut post = Post::new();
+        post.add_text_block("hoge");
+
+        let json = post.to_json();
+        eprintln!{"{}", json}
+        assert_eq!(1, 1);
     }
 }
