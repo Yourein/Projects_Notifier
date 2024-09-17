5fb0e00d2e80570f2bf01794f311162c0ceb9e01
Yourein <rein.you.3.1@gmail.com>
2024-09-17 21:28:52 +0900

    Implement Webhook::post

--- slack/src/webhook.rs
+++ slack/src/webhook.rs
@@ -1,4 +1,5 @@
 use crate::post::Post;
+use reqwest::blocking as reqwestBlocking;
 
 pub struct Webhook {
     end_point: String
@@ -11,7 +12,25 @@ impl Webhook {
         }
     }
 
-    pub fn post(post: Post) -> Result<(), String> {
-        todo!()
+    pub fn post(&self, post: Post) -> Result<(), String> {
+        let Ok(json) = serde_json::to_string(&post) else {
+            return Err("Failed to parse Post".to_string());
+        };
+        
+        let res = reqwestBlocking::Client::new()
+            .post(&self.end_point)
+            .header(reqwest::header::CONTENT_TYPE, "application/json")
+            .body(json)
+            .send();
+        
+        if res.is_ok() {
+            if res.unwrap().status().is_success() {
+                Ok(())
+            } else {
+                Err("Something Wrong!".to_string())
+            }
+        } else {
+            Err("Send Error!".to_string())
+        }
     }
 }
