fe220bb2470acc7b2d505d6ae05b16206990f2cc
Yourein <rein.you.3.1@gmail.com>
2024-09-19 17:36:59 +0900

    Implement DateTime Scaler for gql

---
+++ github/src/scaler.rs
@@ -0,0 +1,18 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Debug)]
+pub(crate) struct DateTime(String);
+
+impl From<&chrono::DateTime<chrono::Utc>> for DateTime {
+    fn from(value: &chrono::DateTime<chrono::Utc>) -> Self {
+        DateTime(value.to_rfc3339())
+    }
+}
+
+impl TryFrom<DateTime> for chrono::DateTime<chrono::Utc> {
+    type Error = chrono::ParseError;
+
+    fn try_from(value: DateTime) -> Result<Self, Self::Error> {
+        chrono::DateTime::parse_from_rfc3339(value.0.as_str()).map(Into::into)
+    }
+}
