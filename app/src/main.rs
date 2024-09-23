001bb45e232c96b5567a5f8a57606ec01601868d
Yourein <rein.you.3.1@gmail.com>
2024-09-23 15:06:02 +0900

    Implement post_task_to_slack

--- app/src/main.rs
+++ app/src/main.rs
@@ -1,3 +1,29 @@
+mod adapter;
+mod redis_wrapper;
+
+use std::env;
+use redis_wrapper::RedisWrapper;
+use github::client::Client;
+use slack::webhook::Webhook;
+use slack::post::Post;
+
 fn main() {
-    println!("Hello, world!");
+    // This app panics when required variables are not found or failed to connect to redis server
+
+    let slack_end_point = env::var("MAIGO_SLACK_URL").unwrap();
+    let github_token = env::var("MAIGO_GITHUB_TOKEN").unwrap();
+    let redis_client = RedisWrapper::new("redis:127.0.0.1").unwrap();
+    let slack = Webhook::new(slack_end_point);
+    let gc = Client::new(&github_token);
+}
+
+fn post_task_to_slack<'a>(client: Webhook, post: Post<'a>) {
+    match client.post(post) {
+        Ok(_) => {
+            println!{"Post Success!"}
+        }
+        Err(_) => {
+            println!{"Post Failed"}
+        }
+    }
 }
